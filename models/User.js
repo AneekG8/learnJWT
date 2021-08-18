df1f40adfd3c41a3c317042df05c9fb7742ee121
aneek ghosh <aneekghosh25@gmail.com>
2021-08-19 05:17:24 +0530

    updated userSchema with custom error messages

--- models/User.js
+++ models/User.js
@@ -1,16 +1,21 @@
-const mongoose  =   require('mongoose');
+const mongoose  =   require('mongoose'),
+      {isEmail} =   require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         lowercase: true,
-        required: true,
-        unique: true
+        required: [true,'email cannot be empty!'],
+        unique: true,
+        validate: {
+            validator: isEmail,
+            message: '{VALUE} is an invalid email'
+        }
     },
     password: {
         type: String,
-        required: true,
-        minLength: 8
+        required: [true,'password cannot be empty'],
+        minLength: [8,'password should be at least 8 characters long']
     }
 })
 
